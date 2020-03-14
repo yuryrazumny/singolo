e047a49af6580aa3614122ae591f64bc154bb9f6
Yuri <yury971@mail.ru>
2020-03-14 20:43:51 +0300

    feat: add change in picture positions to the portfolio && refactor: change screen off && change pop-up window

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const BUTTON = document.getElementById("btn");
 const CLOSE_BUTTON = document.getElementById("close-btn");
 const PORTFOLIO = document.getElementById("portfolio-grid");
 const ONSUBMIT = document.getElementById("onsubmit");
-const iphone1 = document.querySelector(".iphone1");
-const iphone2 = document.querySelector(".iphone2");
+const iphone1 = document.querySelector(".iphone-button1");
+const iphone2 = document.querySelector(".iphone-button2");
+const iphone3 = document.querySelector(".iphone-button3");
 const offscreen1 = document.querySelector(".offscreen1");
 const offscreen2 = document.querySelector(".offscreen2");
+const offscreen3 = document.querySelector(".offscreen3");
 const CHEV_LEFT = document.querySelector(".chev_left");
 const CHEV_RIGHT = document.querySelector(".chev_right");
+const GRID_ITEM = document.querySelectorAll(".grid-item");
 let slider = document.getElementsByClassName("slider");
 let screenoff1 = false;
 let screenoff2 = false;
+let screenoff3 = false;
 
-CHEV_LEFT.addEventListener('click', (e) => {
+CHEV_LEFT.addEventListener('click', () => {
     plusSlides(-1);
     if (slider[0].classList[1] == "blue") {
         slider[0].classList.remove('blue');
@@ -22,7 +26,7 @@ CHEV_LEFT.addEventListener('click', (e) => {
     else { slider[0].classList.add('blue'); }
 });
 
-CHEV_RIGHT.addEventListener('click', (e) => {
+CHEV_RIGHT.addEventListener('click', () => {
     plusSlides(1);
     if (slider[0].classList[1] == "blue") {
         slider[0].classList.remove('blue');
@@ -30,14 +34,19 @@ CHEV_RIGHT.addEventListener('click', (e) => {
     else { slider[0].classList.add('blue'); }
 });
 
-iphone1.addEventListener('click', (e) => {
+iphone1.addEventListener('click', () => {
     screenoff1 = !screenoff1;
     screenoff1 ? offscreen1.style.display = "block" : offscreen1.style.display = "none";
 });
 
-iphone2.addEventListener('click', (e) => {
+iphone2.addEventListener('click', () => {
     screenoff2 = !screenoff2;
     screenoff2 ? offscreen2.style.display = "block" : offscreen2.style.display = "none";
+});
+
+iphone3.addEventListener('click', () => {
+    screenoff3 = !screenoff3;
+    screenoff3 ? offscreen3.style.display = "block" : offscreen3.style.display = "none";
 });
 
 ONSUBMIT.addEventListener('submit', (e) => {
@@ -48,6 +57,29 @@ ONSUBMIT.addEventListener('submit', (e) => {
 MENU_PORTFOLIO.addEventListener('click', (event) => {
     MENU_PORTFOLIO.querySelectorAll('p').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
+    if (event.target.classList[1] == 'b1') {
+        GRID_ITEM[0].classList.remove('last-item');
+        GRID_ITEM[1].classList.remove('last-item');
+        GRID_ITEM[2].classList.remove('last-item');
+    };
+    if (event.target.classList[1] == 'b2') {
+        GRID_ITEM[0].classList.remove('last-item');
+        GRID_ITEM[1].classList.remove('last-item');
+        GRID_ITEM[2].classList.remove('last-item');
+        GRID_ITEM[0].classList.add('last-item');
+    };
+    if (event.target.classList[1] == 'b3') {
+        GRID_ITEM[0].classList.remove('last-item');
+        GRID_ITEM[1].classList.remove('last-item');
+        GRID_ITEM[2].classList.remove('last-item');
+        GRID_ITEM[0].classList.add('last-item');
+        GRID_ITEM[1].classList.add('last-item');
+    };
+    if (event.target.classList[1] == 'b4') {
+        GRID_ITEM[0].classList.add('last-item');
+        GRID_ITEM[1].classList.add('last-item');
+        GRID_ITEM[2].classList.add('last-item');
+    };
 });
 
 PORTFOLIO.addEventListener('click', (event) => {
@@ -61,7 +93,7 @@ MENU.addEventListener('click', (event) => {
 });
 
 BUTTON.addEventListener('click', (e) => {
-    if (document.getElementById('name').value !== '' && document.getElementById('email').value !== '') {
+    if (document.getElementById('name').checkValidity() && document.getElementById('email').checkValidity()) {
         e.preventDefault();
         const subject = document.getElementById('subject').value.toString();
         const project = document.getElementById('project').value.toString();
@@ -77,7 +109,7 @@ BUTTON.addEventListener('click', (e) => {
         }
         else { document.getElementById('result3').innerText = 'Без описания'; }
 
-        document.getElementById('body').classList.add('hide');
+        document.getElementsByTagName('body')[0].classList.add('hide');
         document.getElementById('message-block').classList.remove('hidden');
         document.getElementById('message').classList.remove('hidden');
     }
@@ -87,7 +119,7 @@ CLOSE_BUTTON.addEventListener('click', () => {
     document.getElementById('result1').innerText = '';
     document.getElementById('result2').innerText = '';
     document.getElementById('result3').innerText = '';
-    document.getElementById('body').classList.remove('hide');
+    document.getElementsByTagName('body')[0].classList.remove('hide');
     document.getElementById('message-block').classList.add('hidden');
     document.getElementById('message').classList.add('hidden');
 });
@@ -100,6 +132,8 @@ function plusSlides(n) {
     screenoff1 = false;
     offscreen2.style.display = "none";
     screenoff2 = false;
+    offscreen3.style.display = "none";
+    screenoff3 = false;
     showSlides(slideIndex += n);
 }
 
